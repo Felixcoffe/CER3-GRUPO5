eed3d8b06c5609e20316f8e3da1f9f9bc64a443e
JOlave <jaime.olave@usm.cl>
2022-12-12 18:10:31 -0300

    Tabla

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.db.models import Q
 # Create your views here.
-
+from app.models import Equipo
 
 def home(request):
-    return render(request,'app/home.html')
+    busqueda = request.GET.get("buscar")
+    equipos = Equipo.objects.all()
+    if busqueda:
+        equipos = Equipo.objects.filter(
+                Q(Estado = busqueda)
+            ).distinct()
+    return render(request,'app/home.html',{"equi":equipos})
